0efbe3c655ae1f92eb1b9067a0c4286cf15bd677
Priyalakshmi Nandan <priyalakshmi.nandan@costcutter.com>
2019-05-18 20:58:15 +0100

    TestUpdate

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+Selenium-Cucumber-Maven
+
+Running test
+Go to your project directory from terminal and hit following commands
+
+mvn test (defualt will run on local chrome browser)
+mvn test "-Dbrowser=chrome"
+mvn test "-Dbrowser=firefox"
+mvn test "-Dbrowser=ie" 
--- target/cucumber.html/report.js
+++ target/cucumber.html/report.js
@@ -9,57 +9,81 @@ formatter.scenario({
   "description": "",
   "keyword": "Scenario"
 });
+formatter.before({
+  "status": "passed"
+});
 formatter.step({
   "name": "User launches the BBC Website",
   "keyword": "Given "
 });
-formatter.match({});
+formatter.match({
+  "location": "BBCHomePageSteps.user_launches_the_BBC_Website()"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "passed"
 });
 formatter.step({
   "name": "The WebPage has been loaded compeletly",
   "keyword": "When "
 });
-formatter.match({});
+formatter.match({
+  "location": "BBCHomePageSteps.the_WebPage_has_been_loaded_compeletly()"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "passed"
 });
 formatter.step({
   "name": "Verify whether all Main Menu links are not broken and is navigated to corresponding page or subsection",
   "keyword": "Then "
 });
-formatter.match({});
+formatter.match({
+  "location": "BBCHomePageSteps.verify_whether_all_Menu_links_are_not_broken_and_is_navigated_to_corresponding_page()"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
 });
 formatter.scenario({
   "name": "Launch BBC website \u0026 verify SubMenus in More Menu link",
   "description": "",
   "keyword": "Scenario"
 });
+formatter.before({
+  "status": "passed"
+});
 formatter.step({
   "name": "User launches the BBC Website",
   "keyword": "Given "
 });
-formatter.match({});
+formatter.match({
+  "location": "BBCHomePageSteps.user_launches_the_BBC_Website()"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "passed"
 });
 formatter.step({
   "name": "User have clicked on More Link",
   "keyword": "When "
 });
-formatter.match({});
+formatter.match({
+  "location": "BBCHomePageSteps.user_have_clicked_on_More_Link()"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "passed"
 });
 formatter.step({
   "name": "Verify whether sub Menu links displayed are not broken",
   "keyword": "Then "
 });
-formatter.match({});
+formatter.match({
+  "location": "BBCHomePageSteps.verify_whether_sub_Menu_links_displayed_are_not_broken()"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
 });
 });
